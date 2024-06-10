e75670c22aa4cadcabb15f37e46da5b5fc3a12a6
徳長和希 <tokunagakazuki@tokuchoukazukinoMacBook-Air.local>
2024-06-10 14:28:06 +0900

    second

---
+++ bizpwa.js
@@ -0,0 +1,16 @@
+let pathname = window.location.pathname;
+let startUrl = "";
+if (pathname.startsWith("/manage/agent")){
+  startUrl = "manage/agent";
+}
+else if (pathname.startsWith("/manage/admin")){
+  startUrl = "manage/admin";
+}
+window.progressierAppRuntimeSettings = {
+  startUrl: startUrl,
+}
+console.log("test");
+
+let progressierScript = document.createElement("script");
+progressierScript.setAttribute("src", "https://progressier.app/tTD4oTBMz4ZuQAhT7HeQ/script.js");
+document.querySelector("head").appendChild(progressierScript);
